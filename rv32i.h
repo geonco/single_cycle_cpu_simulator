35c0d20f855e14de10bf4a1ead523e7d9220980d
geonwoolim <geonco0709@gmail.com>
2026-06-26 18:02:41 +0900

    Add datapath structs to rv32i.h

--- rv32i.h
+++ rv32i.h
@@ -24,7 +24,41 @@
 struct imem_input_t {
 	uint32_t addr;
 };
-
 struct imem_output_t {
+	uint32_t dout;
+};
+
+struct regfile_input_t {
+	uint32_t rs1;
+	uint32_t rs2;
+	uint32_t rd;
+	uint32_t rd_din;
+	uint32_t reg_write;
+};
+struct regfile_output_t {
+	uint32_t rs1_dout;
+	uint32_t rs2_dout;
+};
+
+struct alu_input_t {
+	uint32_t in1;
+	uint32_t in2;
+	uint32_t alu_control;		
+};
+struct alu_output_t {
+	uint32_t result;
+	uint32_t zero;
+	uint32_t sign;		
+};
+
+// unaligned dmem
+struct dmem_input_t {
+	uint32_t addr;
+	uint32_t rd_en;		// mem_read
+	uint32_t wr_en;		// mem_write
+	uint32_t sz;
+	uint32_t din;
+};
+struct dmem_output_t {
 	uint32_t dout;
 };
